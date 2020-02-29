f5aed5f9376e63eaf4ec3a661acce685b573204f
Warren Hu <me@warrenhu.com>
2020-02-28 20:01:50 -0800

    Workin

--- src/main.cpp
+++ src/main.cpp
@@ -60,37 +60,59 @@ ScatterEvent SampleVolume(const Ray ray, Rng& rng, Volume v, PLF& plf) {
     return result;
 }
 
-float3 trace_ray(const float3 eye, float3 target, Rng& rng, Volume& volume, PLF plf) {
-    // Generate ray
-    const float3 direction = normalize(target - eye);
-    Ray ray = { direction, eye };
+float3 shade_surface(Ray& ray, Rng& rng, ScatterEvent& hit, float3& throughput) {
+    // Tangent space basis vectors
+    float3 normal = hit.gradient;
+    float3 tangent = hit.tangent;
+    float3 bitangent = cross(normal, tangent);
+    
+    // Evaluate the BRDF
+    float3 wo;
+    float probability;
+    float3 wi = -ray.direction;
+    float3 wi_t = float3(tangent.x * wi.x + tangent.y * wi.y + tangent.z * wi.z,
+                         normal.x * wi.x + normal.y * wi.y + normal.z * wi.z,
+                         bitangent.x * wi.x + bitangent.y * wi.y + bitangent.z * wi.z);
+    float3 brdf = hit.mat.Sample(wi_t, float2(rng.generate(), rng.generate()), wo, probability);
 
+    // If material is an emitter, then it's light source and we can stop bouncing
+    if (hit.mat.Emission.x > 0 || hit.mat.Emission.y > 0 || hit.mat.Emission.z > 0) {
+        float3 radiance = throughput * hit.mat.Emission; // * weight;
+        throughput = 0.f;
+
+        return radiance;
+    }
+
+    float3 radiance = 0.f;
+
+    // Send a ray to a random light in the scene (handle direct lighting)
+    if (false) { // If # lights > 0
+        // then sample a random light
+        // and set the radiance to the light and material properties
+    }
+
+    // Next bounce (for additional indirect lighting)
+    ray.origin = hit.position;
+    ray.direction = normalize(float3(tangent.x * wo.x + normal.x * wo.y + bitangent.x * wo.z,
+                                     tangent.y * wo.x + normal.y * wo.y + bitangent.y * wo.z,
+                                     tangent.z * wo.x + normal.z * wo.y + bitangent.z * wo.z));
+
+    // Keep track of the evalulated material until we hit a light source
+    throughput *= abs(dot(normal, ray.direction)) * brdf / probability;
+
+    // If we didn't hit a light, radiance is zero. Otherwise, returns the radiance from the light
+    return clamp(radiance, 0, 3);
+}
+
+float3 trace_ray(Ray ray, Rng& rng, Volume volume, PLF plf) {
     // Scene intersection
     ScatterEvent hit = SampleVolume(ray, rng, volume, plf);
     if (!hit.valid) {
         return 0;
     }
 
-    float3 normal = hit.gradient;
-    float3 tangent = hit.tangent;
-    float3 bitangent = cross(normal, tangent);
-    
-    float3 wi = -ray.direction;
-    float3 wi_t = float3(tangent.x * wi.x + normal.x * wi.x + bitangent.x * wi.x,
-                         tangent.y * wi.y + normal.y * wi.y + bitangent.y * wi.y,
-                         tangent.z * wi.z + normal.z * wi.z + bitangent.z * wi.z);
-    float3 outgoing_angle;
-    float probability;
-    float3 brdf = hit.mat.Sample(wi_t, float2(rng.generate(), rng.generate()), outgoing_angle, probability);
-
-    float throughput = 1.f;
-    if (hit.mat.Emission.x > 0 || hit.mat.Emission.y > 0 || hit.mat.Emission.z > 0) {
-        return throughput * hit.mat.Emission; // * weight;
-    }
-
-    float3 radiance = 0.f;
-
-    return throughput; // + lights;
+    // Shade the surface that was hit
+    float3 radiance = shade_surface(ray, rng, hit, throughput);
 }
 
 PLF get_transfer_function() {
@@ -145,12 +167,15 @@ int main(int argc, char** argv) {
     float3* image = new float3[OUTPUT_WIDTH * OUTPUT_HEIGHT];
     for (uint32_t x = 0; x < OUTPUT_WIDTH; x++) {
         for (uint32_t y = 0; y < OUTPUT_HEIGHT; y++) {
+            // Generate ray
             const float3 eye = float3(0.f, 0.f, -1.f);
             const float target_x = ((2.f * (float)x / (float)OUTPUT_WIDTH) - 1.f);
             const float target_y = ((2.f * (float)y / (float)OUTPUT_HEIGHT) - 1.f);
             const float3 target = float3(target_x, target_y, eye.z + 4.0f);
+            const float3 direction = normalize(target - eye);
+            Ray ray = { direction, eye };
 
-            float3 hdr_color = trace_ray(eye, target, rng, d.volume, plf);
+            float3 hdr_color = trace_ray(ray, rng, d.volume, plf);
 
             float3 ldr = tonemap_aces(hdr_color);
             image[x + (y * OUTPUT_HEIGHT)] = pow(ldr, 2.2f);
